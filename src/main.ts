642fa043bff7e47e58c744a52b39aea2ea3e72d8
Fabricio Cardoso <fabricio.cardoso@vizir.com.br>
2022-12-11 09:47:28 -0300

    chore: adjust openApi description

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
+    .setTitle('MyCMS')
+    .setDescription('MyCMS api test interface')
     .setVersion('1.0')
-    .addTag('cats')
+    .addTag('myCMS')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
